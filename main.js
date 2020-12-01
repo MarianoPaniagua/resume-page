81741de2c7657baae69dfec8a162eb59bab28244
Mariano Paniagua <mpaniagua@contractor.ea.com>
2020-12-01 17:17:26 -0300

    Added fetch to heroku

---
+++ main.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("downloadbtn").addEventListener("click", count);
+
+    function count() {
+        fetch('https://resume-visit-counter.herokuapp.com/info')
+            .then(response => {
+                if (response.ok) {
+                    console.log("Thanks for downloading my resume :D")
+                }
+            }).catch(error => console.log("Ups there was a problem counting this, don´t worry, enjoy my resume :D"))
+    }
+})
